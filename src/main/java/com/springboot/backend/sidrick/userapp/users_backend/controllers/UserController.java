136d2c58f62c42522954c16bf686110d32909cec
Sergey Brovko <serbro7@gmail.com>
2025-11-05 18:28:08 +0100

    CORS policy solution - CrossOrigin annotation added

--- src/main/java/com/springboot/backend/sidrick/userapp/users_backend/controllers/UserController.java
+++ src/main/java/com/springboot/backend/sidrick/userapp/users_backend/controllers/UserController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,8 +22,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
-
-
+//The @CrossOrigin annotation is added to fix the CORS policy blocking error in our front-end.
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
