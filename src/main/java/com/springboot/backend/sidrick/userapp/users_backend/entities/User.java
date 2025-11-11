a2a01235be5bf06a5c5fb1694a7b21157d310b3b
Sergey Brovko <serbro7@gmail.com>
2025-11-11 16:51:51 +0100

    Role Entity. Many to many relationship between User and Role

--- src/main/java/com/springboot/backend/sidrick/userapp/users_backend/entities/User.java
+++ src/main/java/com/springboot/backend/sidrick/userapp/users_backend/entities/User.java
@@ -1,14 +1,26 @@
 package com.springboot.backend.sidrick.userapp.users_backend.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import static jakarta.persistence.GenerationType.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import net.bytebuddy.dynamic.TypeResolutionStrategy.Lazy;
 
 //By convention, the tables in databases are called in plural, whereas the Java Entity class is called in
 //singular, because the Entity is an instance.
@@ -41,6 +53,28 @@ public class User {
 
     @NotBlank
     private String password;
+
+    //Since we've just created the Role Entity, now we must indicate that User entity is linked to it somehow. A user can
+    //have one or more Roles, and each Role can have one or more user, so the relationoship is many-to-many. In order to join
+    //the User table and the Roles table, we had do create an intermediate table called users_roles. User entity is related to 
+    //users_roles through the 'user_id' column (hence the joinColumns) and the Roles entity does the same thing through the 'role_id', 
+    //(hence the inverseJoinColumns).
+    //@JsonIgnorePoperties ignores garbage properties that can't be converted to JSON
+    //@ManyToMany annotation specifies that FetchType is Lazy in order not to retrieve every single table relationship that the User
+    //might have, unless those relationships are explicitly requested.
+    @JsonIgnoreProperties({"handler", "hibernateLazyInitializer"})
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name="users_roles",
+        joinColumns = {@JoinColumn(name="user_id")},
+        inverseJoinColumns = {@JoinColumn(name="role_id")},
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role_id"})}
+    )
+    private List<Role> roles;
+
+    public User() {
+        this.roles = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -78,5 +112,15 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    
     
 }
