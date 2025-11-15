b6ccd70fcd218920b0eccd0ad52f3b081d49870d
Sergey Brovko <serbro7@gmail.com>
2025-11-15 16:56:15 +0100

    JWT Authentication filter created and basic configuration

---
+++ src/main/java/com/springboot/backend/sidrick/userapp/users_backend/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package com.springboot.backend.sidrick.userapp.users_backend.auth.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.backend.sidrick.userapp.users_backend.entities.User;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//The purpose of this Class is to retrieve a User wiht their respective username and password.
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    private AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+
+        String username = null;
+        String password = null;
+
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            username = user.getUsername();
+            password = user.getPassword();
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        return this.authenticationManager.authenticate(authenticationToken);
+    }
+    
+
+}
