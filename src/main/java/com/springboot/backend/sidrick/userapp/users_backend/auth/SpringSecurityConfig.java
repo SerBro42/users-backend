55c379ae147c5625ac55ddb75f37bcc235610d10
Sergey Brovko <serbro7@gmail.com>
2025-11-15 16:27:54 +0100

    Configured security rules for GET, POST, PUT, DELETE endpoints

--- src/main/java/com/springboot/backend/sidrick/userapp/users_backend/auth/SpringSecurityConfig.java
+++ src/main/java/com/springboot/backend/sidrick/userapp/users_backend/auth/SpringSecurityConfig.java
@@ -10,13 +10,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SpringSecurityConfig {
 
+    private final SecurityFilterChain filterChain;
+
+    SpringSecurityConfig(SecurityFilterChain filterChain) {
+        this.filterChain = filterChain;
+    }
+
     //We annotate it as a Bean so that it gets registered in the Spring Container as a Component.
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        //With these configurations, all routes will be protected except /api/users and /api/users/page/{page}
+        //The first line means that the routes /api/users and /api/users/page/{page} are accessible to anyone, regardless of whether they're logged in or not.
+        //The second requestMatchers() line means that a user details by ID can be viewed by logged users of any type.
+        //The subsequent requestMatchers() lines mean that POST, PUT and DELETE requests can be issued only by logged in ADMIN users.
         return http.authorizeHttpRequests( authz -> authz
         .requestMatchers(HttpMethod.GET, "/api/users", "/api/users/page/**").permitAll()
+        .requestMatchers(HttpMethod.GET, "/api/users/{id}").hasAnyRole("USER", "ADMIN")
+        .requestMatchers(HttpMethod.POST, "/api/users").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.PUT, "/api/users/{id}").hasRole("ADMIN")
+        .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").hasRole("ADMIN")
             .anyRequest().authenticated()
         )
         .csrf(config -> config.disable())
