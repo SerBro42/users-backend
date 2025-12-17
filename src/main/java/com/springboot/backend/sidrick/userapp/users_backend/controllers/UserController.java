ef9e69dd3b5d685fd06baec05ecdc3108cd3a50b
Sergey Brovko <serbro7@gmail.com>
2025-12-17 18:47:14 +0100

    Enabled creation of JAR files with Gradle

--- src/main/java/com/springboot/backend/sidrick/userapp/users_backend/controllers/UserController.java
+++ src/main/java/com/springboot/backend/sidrick/userapp/users_backend/controllers/UserController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 //The @CrossOrigin annotation is added to fix the CORS policy blocking error in our front-end.
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200"}, originPatterns = {"*"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
